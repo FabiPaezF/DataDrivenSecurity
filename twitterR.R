015f0d7355546d4a90bb1977d87092591e717f34
PedroGalindo <pedrogalindomorey@hotmail.com>
2017-12-11 19:40:26 +0100

    Fix twitter.R first commit

---
+++ twitterR.R
@@ -0,0 +1,13 @@
+require(twitteR)
+require(RCurl)
+
+consumer_key <- 'PUZuo6k75lZYGQBRiwlJLc9Og'
+consumer_secret <- '4My1RZHZgbYvaA5XBIAcJ11dqpgQtDOEecHR3LoZFcouCr1nL7'
+access_token <- '938134932383260672-vkIaphVYLXb2tnnpKFJU3calS4pE4hu'
+access_secret <- 'ZNscFMCMQX9ercbLgN16ROrsFULA0GX04k0BMgS7VvnAQ'
+
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+1
+search <- searchTwitter('wannacry', geocode='40.7361,-73.9901,5mi',  n=5000, retryOnRateLimit=1)
+
+print(search)
