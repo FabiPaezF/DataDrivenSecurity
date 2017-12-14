6f7a54a62696fbcace49ee945fc28b790af3142e
FabiPaezF <34250539+FabiPaezF@users.noreply.github.com>
2017-12-14 18:49:37 +0100

    Add files via upload

---
+++ search.R
@@ -0,0 +1,16 @@
+require(twitteR)
+require(RCurl)
+
+consumer_key <- 'PUZuo6k75lZYGQBRiwlJLc9Og'
+consumer_secret <- '4My1RZHZgbYvaA5XBIAcJ11dqpgQtDOEecHR3LoZFcouCr1nL7'
+access_token <- '938134932383260672-vkIaphVYLXb2tnnpKFJU3calS4pE4hu'
+access_secret <- 'ZNscFMCMQX9ercbLgN16ROrsFULA0GX04k0BMgS7VvnAQ'
+n, access_secret)
+1
+setup_twitter_oauth(consumer_key, consumer_secret, access_toke
+
+for (x in 1:100){
+  searchResults <- searchTwitter('wannacry', since='2017-05-07', sinceID=x*5000, n=5000)
+  tweetFrame <- twListToDF(searchResults)
+  write.csv(tweetFrame, file = paste("./Wannacry/wannacry",x,".csv", sep=""))
+}
