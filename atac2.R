f87e55b88064352d5293f549b990cd1423aa1fe9
Pedro Galindo Morey <pedrogalindomorey@hotmail.com>
2017-12-17 01:13:23 +0100

    Add RepeatedWords grafs and others

--- atac2.R
+++ atac2.R
@@ -10,10 +10,10 @@ setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 1
 
 ltw <- list()
-for (x in 1:100){
-  searchResults <- searchTwitter('wannacry', since='2017-05-07', sinceID=x*5000, n=5000)
+for (x in 1:5){
+  searchResults <- searchTwitter('DDoS Playstation', since='2017-05-07', sinceID=x*5000, n=5000)
   ltw[[x]] <- twListToDF(searchResults)
 }
 df.tw <- plyr::ldply(ltw)
 #nomestw<-df.tw %>% filter(isRetweet == FALSE)
-saveRDS(df.tw, file = "wannacry.rds")
+saveRDS(df.tw, file = "playstation.rds")
--- graficPaisosTweets.R
+++ graficPaisosTweets.R
@@ -9,10 +9,14 @@ revisarPaquetes <- function(pkg){
 paquetsNecessaris<-c("twitteR", "RCurl", "dismo", "maps", "ggplot2", "XML") #paquetes necesarios
 revisarPaquetes(paquetsNecessaris) #llamamos a la funcion creada
 
-consumer_key <- 'PUZuo6k75lZYGQBRiwlJLc9Og'
-consumer_secret <- '4My1RZHZgbYvaA5XBIAcJ11dqpgQtDOEecHR3LoZFcouCr1nL7'
-access_token <- '938134932383260672-vkIaphVYLXb2tnnpKFJU3calS4pE4hu'
-access_secret <- 'ZNscFMCMQX9ercbLgN16ROrsFULA0GX04k0BMgS7VvnAQ'
+#consumer_key <- 'PUZuo6k75lZYGQBRiwlJLc9Og'
+#consumer_secret <- '4My1RZHZgbYvaA5XBIAcJ11dqpgQtDOEecHR3LoZFcouCr1nL7'
+#access_token <- '938134932383260672-vkIaphVYLXb2tnnpKFJU3calS4pE4hu'
+#access_secret <- 'ZNscFMCMQX9ercbLgN16ROrsFULA0GX04k0BMgS7VvnAQ'
+consumer_key <- 'xTtWssMDK0lbL3snyh07qc6Tq'
+consumer_secret <- 'uhFU4p4XAijNMaKXml9dqatYN09AhWKU2XgNFgDkYIPEMMjafe'
+access_token <- '942082776689528832-p8iBl60ho1aYTNmwOFxgOIKF1TrSeLo'
+access_secret <- 'kadqQQ0YqP5iGzgp9MkfP4pDRM6rDXrL9N7khPlPqcgv5'
 
 setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret) #accesso a Twitter
 1 ##para confirmar la autenticacion
@@ -25,14 +29,3 @@ locatedUsers <- !is.na(userFrame$location)  # Guardamos la informacion relativa 
 
 locations <- geocode(userFrame$location[locatedUsers])  # API para conseguir la localizacion aproximada de los usuarios a partir de latitud i longitud.
 with(locations, plot(longitude, latitude))
-
-worldMap <- map_data("world")  # MAPAMUNDI
-
-zp1 <- ggplot(worldMap) # Pintamos el mapa
-#tratamos el mapa
-zp1 <- zp1 + geom_path(aes(x = long, y = lat, group = group), colour = gray(2/3), lwd = 1/3) 
-# Afegim un punt per usuari
-zp1 <- zp1 + geom_point(data = locations, aes(x = longitude, y = latitude), colour = "RED", alpha = 1/2, size = 1/3)
-zp1 <- zp1 + coord_equal() # permite conservar la relacion altura ancho (proporciones)
-zp1 <- zp1 + theme_minimal()  # quitar información no útil del fondo
-print(zp1)
